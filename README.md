342c45fa3c8eca172e80b15bf66d1a7afe567ccc
Miguel Chan <vvchan@outlook.com>
2019-05-20 01:11:55 +0800

    Add parallel performance test

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 A Rust crate implementing regression decision tree and boosting ensemble.
 
+## Bins
+
+**cross validation**:
+
+```
+cargo run --bin cv
+```
+
+**predict test set**:
+
+```
+cargo run --bin predict
+```
+
+**test parallel performance**:
+
+```shell
+for ((i=1; i<=12 ; i++)) cargo run --release --bin parallel_performance $i 
+```
+
+
 
 ## References
 
@@ -9,4 +30,4 @@ A Rust crate implementing regression decision tree and boosting ensemble.
 
 [Efficient Determination of Dynamic Split Points in a Decision Tree](https://www.microsoft.com/en-us/research/publication/efficient-determination-dynamic-split-points-decision-tree/?from=http%3A%2F%2Fresearch.microsoft.com%2Fen-us%2Fum%2Fpeople%2Fdmax%2Fpublications%2Fsplits.pdf)
 
-
+[Parallel Gradient Boosting Decision Trees](https://zhanpengfang.github.io/418home.html)
--- src/bin/parallel_performance.rs
+++ src/bin/parallel_performance.rs
@@ -1,10 +1,12 @@
 use ensembles_rs::data_frame;
+use ensembles_rs::learner::Learner;
 use ensembles_rs::tree;
 use ensembles_rs::tree::DecisionTreeConfig;
-use ensembles_rs::utils::cross_validate;
 use ensembles_rs::utils::numeric;
+use ensembles_rs::utils::set_num_threads;
 use rayon::prelude::*;
 use std::collections::HashSet;
+use num_cpus;
 
 use log::*;
 
@@ -12,17 +14,29 @@ use pretty_env_logger;
 
 use std::path::*;
 use std::time;
+use std::env;
 
 static DATA_DIR: &str = "../data";
 
 fn main() {
     pretty_env_logger::init();
 
+    // Get threads number from cli params
+    let params: Vec<String> = env::args().collect();
+    let first_params = params.get(1);
+    let threads = match first_params {
+        Some(content) => content.parse::<usize>().unwrap(),
+        None => num_cpus::get()
+    };
+    // Test different threads performance
+    println!("Using {} threads", threads);
+    set_num_threads(threads);
+
     let data_path = Path::new(DATA_DIR);
     let start = time::SystemTime::now();
     debug!("Loading Train Data");
     let train_data = data_frame::read_csvs(
-        (1..6)
+        (1..2)
             .into_par_iter()
             .map(|index| -> PathBuf { data_path.join(format!("train{}.csv", index)) })
             .collect(),
@@ -30,7 +44,7 @@ fn main() {
     info!("Train data shape: {:?}", train_data.shape());
     debug!("Loading Label Data");
     let label_data = data_frame::read_csvs(
-        (1..6)
+        (1..2)
             .into_par_iter()
             .map(|index| -> PathBuf { data_path.join(format!("label{}.csv", index)) })
             .collect(),
@@ -39,7 +53,7 @@ fn main() {
     let label_data = label_data.into_shape([1 as usize, samples_count]).unwrap();
     info!("Label data shape: {:?}", label_data.shape());
 
-    info!("Load time: {}ms", start.elapsed().unwrap().as_millis());
+    println!("Load time: {}ms", start.elapsed().unwrap().as_millis());
 
     let tree_config = {
         let mut configs = HashSet::new();
@@ -47,15 +61,24 @@ fn main() {
         configs.insert(DecisionTreeConfig::MinSamplesSplit(
             samples_count / 10000000,
         ));
-        configs.insert(DecisionTreeConfig::MaxBin(300));
-        configs.insert(DecisionTreeConfig::MaxDepth(10));
+        configs.insert(DecisionTreeConfig::MaxBin(100));
+        configs.insert(DecisionTreeConfig::MaxDepth(3));
         configs
     };
 
     let tree = tree::DecisionTree::new_with_config(tree_config);
 
-    let folds = cross_validate::KFold::new(3);
-    let results = folds.cross_validate(tree, &train_data, &label_data, numeric::r2_score);
-    println!("{:?}", results);
-    //    tree.fit(&train_data, &label_data);
+
+    {
+        let mut model = tree.clone();
+        let start = time::SystemTime::now();
+        model.fit(&train_data, &label_data);
+        println!("{} threads training time: {}ms", threads, start.elapsed().unwrap().as_millis());
+        let start = time::SystemTime::now();
+        let result = model.predict(&train_data);
+        println!("Train score: {}", numeric::r2_score(&label_data, &result));
+        println!("{} threads predict time: {}ms", threads, start.elapsed().unwrap().as_millis());
+    }
+
+
 }
